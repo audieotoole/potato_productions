8c4e85aabca51ba66089955cdf0eede1443681a4
Audie O'Toole <audieotoole@gmail.com>
2021-08-03 21:12:02 -0600

    removed unused import

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
